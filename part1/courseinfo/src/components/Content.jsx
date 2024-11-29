3532c9a45511bf682aa86ef0391b48a0fd096574
Toshiro Mendoza <122713720+InZaneManInAZuite@users.noreply.github.com>
2024-11-30 01:58:51 +1300

    fix: handle error due to content mapped not having key for courseinfo

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -3,7 +3,7 @@ import Part from './Part'
 const Content = (props) => {
 
     return (
-        props.parts.map(part => <Part part={part}/>)
+        props.parts.map(part => <Part key={part.name} part={part}/>)
     )
 }
 
