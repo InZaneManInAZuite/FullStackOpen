02ccf041727c18f76006f77c1fe848512e9d87c8
Toshiro Mendoza <122713720+InZaneManInAZuite@users.noreply.github.com>
2024-12-06 02:14:50 +1300

    add: done with exercise 2.1

---
+++ part2/courseinfo/src/components/Total.jsx
@@ -0,0 +1,13 @@
+const Total = (props) => {
+
+    let total = 0
+    props.parts.forEach(part => {
+        total += part.exercises
+    })
+
+    return (
+        <p>Number of exercises {total}</p>
+    )
+}
+
+export default Total
